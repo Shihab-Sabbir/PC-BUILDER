5518aa98fc74cdf196f0d4b2567e52112df6ef59
shihab-sabbir <shihab11231@gmail.com>
2023-07-30 00:59:46 +0600

    update featured product fetching

--- src/components/UI/FeaturedProducts.js
+++ src/components/UI/FeaturedProducts.js
@@ -2,16 +2,19 @@
 import ProductCard from "./ProductCard";
 
 export default function FeaturedProducts({ products }) {
+    const featuredProducts = products?.filter(product => product?.featured);
+
     return (
         <div className="p-2 lg:p-10">
             <p className='text-xl font-bold text-center'>Featured Products</p>
             <p className='pt-[20px] pb-[50px] font-thin text-lg font-[calibri] text-center'>Check & Get Your Desired Product!</p>
             <div className='grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
-                {products?.map(product => <div key={product._id}>
-                    {product?.featured && <ProductCard product={product}/>}
-                </div>)}
+                {featuredProducts?.map(product => (
+                    <div key={product._id}>
+                        <ProductCard product={product} />
+                    </div>
+                ))}
             </div>
         </div>
     )
 }
-
